c66ff1e7498bad55479bb1e922caf5c27bf5ef73
Steve Testa <steve@testasoftware.com>
2024-09-10 15:40:18 -0400

    Initial Customization
    - colorschemes
    - dashboard logo
    - supertab

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  -- Various themes
+  { "dracula/vim", lazy = true, name = "dracula" },
+  { "navarasu/onedark.nvim", lazy = true, name = "onedark" },
+  { "projekt0n/github-nvim-theme", lazy = true, name = "github" },
+  { "baliestri/aura-theme", lazy = true, name = "aura" },
+  { "rose-pine/neovim", lazy = true, name = "rose-pine" },
+  { "EdenEast/nightfox.nvim", lazy = true, name = "nightfox" },
+  { "rebelot/kanagawa.nvim", lazy = true, name = "kanagawa" },
+
+  -- Flavor of the day
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+      style = "moon",
+    },
+  },
+}
