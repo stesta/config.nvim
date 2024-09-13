11ee273abcce61033890b9633e31df089d8cbdf9
Steve Testa <steve@testasoftware.com>
2024-09-12 21:51:51 -0400

    added vscode color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,13 +7,61 @@ return {
   { "rose-pine/neovim", lazy = true, name = "rose-pine" },
   { "EdenEast/nightfox.nvim", lazy = true, name = "nightfox" },
   { "rebelot/kanagawa.nvim", lazy = true, name = "kanagawa" },
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = true,
+    name = "vscode",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    integrations = {
+      aerial = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      flash = true,
+      grug_far = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      markdown = true,
+      mini = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      semantic_tokens = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
 
   -- Flavor of the day
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
-      style = "moon",
+      colorscheme = "vscode",
+      style = "dark",
     },
   },
 }
